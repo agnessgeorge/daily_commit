7a6295c6520bb1670f7599f4279fe9a86e87f0d6
agnessgeorge <agnessgeorge8@icloud.com>
2021-04-19 15:37:29 +0800

    Create daily_commit_20210419_python_tuples_sets.py
    Python Data Structure Quiz:Tuples, Sets
    a)What is the major difference between tuples and lists?
    b)How do you create a tuple?
    c)What is unique about a set?
    d)Use a set to find the unique values of the list below:[1, 2, 2, 33, 4, 4, 11, 22, 3, 3, 2]

---
+++ daily_commit_20210419_python_tuples_sets.py
@@ -0,0 +1,16 @@
+# 4.19 monday
+# Tuples and sets
+
+# What is the major difference between tuples and lists?
+# You cannot use append method on a tuple, tuples are immutable
+
+# How do you create a tuple?
+t = ()
+
+# Sets
+# What is unique about a set?
+# set's cannot take duplicate items
+
+# Use a set to find the unique values of the list below:
+list5 = [1, 2, 2, 33, 4, 4, 11, 22, 3, 3, 2]
+(set(list5))
