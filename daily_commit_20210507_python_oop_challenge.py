3a23735142837d18cfcda6ebd101d08e49fed18e
agnessgeorge <agnessgeorge8@icloud.com>
2021-05-08 23:59:18 +0800

    Update daily_commit_20210507_python_oop_challenge.py
    UPDATE

--- daily_commit_20210507_python_oop_challenge.py
+++ daily_commit_20210507_python_oop_challenge.py
@@ -1,17 +1,3 @@
-# Object Oriented Programming Challenge
-
-# For this challenge, create a bank account class that has two attributes:
-#* owner
-#* balance
-
-#and two methods:
-
-#* deposit
-#* withdraw
-
-#As an added requirement, withdrawals may not exceed the available balance.
-#Instantiate your class, make several deposits and withdrawals, and test to make sure the account can't be overdrawn.
-
 class Account:
        
     deposits = 0
