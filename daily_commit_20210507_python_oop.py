e0fabdb06e0d593ac6c19a18f58a1e5a87fe11e5
agnessgeorge <agnessgeorge8@icloud.com>
2021-05-10 22:05:20 +0800

    Update daily_commit_20210507_python_oop.py

--- daily_commit_20210507_python_oop.py
+++ daily_commit_20210507_python_oop.py
@@ -1,5 +1,4 @@
 # Object Oriented Programming
-## Homework Assignment
 
 #### Problem 1
 #Fill in the Line class methods to accept coordinates as a pair of tuples and return the slope and distance of the line.
