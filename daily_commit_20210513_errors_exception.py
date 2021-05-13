c4fa2cac6d9824dac4e45da39828541a6946c8fe
agnessgeorge <agnessgeorge8@icloud.com>
2021-05-13 11:51:26 +0800

    Create daily_commit_20210513_errors_exception.py
    Python Errors and Exception Handling:
    Problem 1:
    Handle the exception thrown by the code below by using try and except blocks.
    for i in ['a','b','c']:
        print(i**2)
    
    Problem 2:
    Handle the exception thrown by the code below by using try and except blocks. Then use a finally block to print 'All Done.'
    x = 5
    y = 0
    z = x/y
    
    Problem 3:
    Write a function that asks for an integer and prints the square of it. Use a while loop with a try, except, else block to account for incorrect inputs.
    def ask():
        pass

---
+++ daily_commit_20210513_errors_exception.py
@@ -0,0 +1,40 @@
+#1
+try:
+    for i in ['a','b','c']:
+        print(i**2)
+except:
+    print ("operand not supported")
+
+#2
+x = 5
+y = 0
+ 
+try :
+    x = 5
+    y = 0
+
+    z = x/y
+    print(z)
+except:
+    print("Impossible , division by zero")
+finally:
+    y = input("enter a new non-zero divisor: ")
+    z = x/y
+    print(z)
+    print ("All Done!")
+
+#3
+def ask():
+    while True:
+        try:
+            a = int(input("Input an integer: "))
+        except:
+            print("Looks like you did not enter an integer!")
+        else:
+            print("Yep that's an integer!")
+            break
+        
+    print("Thank you, your number squared is:  %d"%(a**2))
+
+
+ask()
